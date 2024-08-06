db78a4fd8583299e099302e8c7b3b9992d191bcb
Zahraa Al-Hussaini <zhori1993@gmail.com>
2024-08-06 20:46:51 +0300

    task

---
+++  arrays1.js
@@ -0,0 +1,17 @@
+//1//
+let groceries = ["Apple", "orange", "bag", "potato", "onion", "salt"];
+//2//
+console.log(groceries[1]);
+//3//
+console.log(groceries.length);
+//4//
+console.log(groceries[5]);
+//5//
+let result = groceries.pop();
+console.log(result);
+//6//
+let results = groceries.push("soap", "brush");
+console.log(results);
+//7//
+let firstThreeItems = [groceries.slice(0, 3)];
+console.log(firstThreeItems);
